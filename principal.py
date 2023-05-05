3be11d62a228ce748677a7edb96899cb8d832de5
marcos-gma <127634552+marcos-gma@users.noreply.github.com>
2023-05-04 21:44:14 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -39,14 +39,18 @@ SONIC_IMG = pygame.transform.scale(SONIC_IMG, (LARG_PERS,ALTU_PERS))
 KNUCKLES_IMG = pygame.image.load(os.path.join('sprites', 'knuckles.png'))
 KNUCKLES_IMG = pygame.transform.flip(pygame.transform.scale(KNUCKLES_IMG, (LARG_PERS,ALTU_PERS)), True, False)
 
+
 class Player(pygame.sprite.Sprite):
     COR = (255, 0, 0)
+    
+    SONIC_IMG = pygame.image.load(os.path.join('sprites', 'sonic.png'))
+    SPRITE = pygame.transform.scale(SONIC_IMG, (LARG_PERS,ALTU_PERS))
 
     def __init__(self, x, y, largura, altura):
+        super().__init__()
         self.rect = pygame.Rect(x, y, largura, altura)
         self.x_vel = 0
         self.y_vel = 0
-        self.mask = None
         self.direcao = "esquerda"
         self.animacao_cont = 0
 
@@ -82,17 +86,37 @@ class Player(pygame.sprite.Sprite):
         self.movimento(self.x_vel, self.y_vel)
 
     def draw(self, janela):
-        pygame.draw.rect(janela, self.COR, self.rect)
-        
+        self.sprite = self.SPRITE
+        janela.blit(self.sprite, (self.rect.x,self.rect.y))
 
-def draw_janela(vermelho,azul,texto_formatado, background, player):
+class Objeto(pygame.sprite.Sprite):
+
+    ANEL = pygame.image.load(os.path.join('sprites','aneis', 'ANEL1.png'))
+    SPRITE = pygame.transform.scale(ANEL, (30,30))
+
+    def __init__(self, x, y, largura, altura, nome=None):
+        super().__init__()
+        self.imagem = pygame.Surface((largura,altura), pygame.SRCALPHA)
+        self.rect = pygame.Rect(x, y, largura, altura)
+        self.largura = largura
+        self.altura = altura
+        self.nome = nome
+        self.image_files = ('ANEL1.png','ANEL2.png')
+
+    def draw(self, janela):
+        self.sprite = self.SPRITE
+        janela.blit(self.sprite, (self.rect.x, self.rect.y))
+
+    def update(self, collided):
+        if collided:
+            self.kill()
+
+def draw_janela(texto_formatado, background, player, objeto):
 
     JANELA.fill(PRETO)
     JANELA.blit(background, (0,0))
-    #JANELA.blit(KNUCKLES_IMG, (vermelho.x,vermelho.y))
-    #JANELA.blit(SONIC_IMG, (azul.x,azul.y))
     JANELA.blit(texto_formatado, (450, 40))
-
+    objeto.draw(JANELA)
     player.draw(JANELA)
 
     pygame.display.update()
@@ -103,38 +127,22 @@ def mover(player):
     player.x_vel = 0
     player.y_vel = 0
 
-    if keys[pygame.K_a]:
+    if keys[pygame.K_a] and player.rect.x > 0:
         player.esquerda(VEL)
-    if keys[pygame.K_d]:
+    if keys[pygame.K_d] and player.rect.x < 600:
         player.direita(VEL)
-    if keys[pygame.K_w]:
+    if keys[pygame.K_w] and player.rect.y > 0:
         player.cima(VEL)
-    if keys[pygame.K_s]:
+    if keys[pygame.K_s] and player.rect.y < 530:
         player.baixo(VEL)
 
-def vermelho_mov(keys_pressed,vermelho):
-    if keys_pressed[pygame.K_a] and vermelho.x - VEL > 0: #ESQUERDA
-        vermelho.x -= VEL
-    if keys_pressed[pygame.K_d] and vermelho.x + VEL + vermelho.width < LARGURA: #DIREITA
-        vermelho.x += VEL
-    if keys_pressed[pygame.K_w] and vermelho.y - VEL > 0: #CIMA
-        vermelho.y -= VEL
-    if keys_pressed[pygame.K_s] and vermelho.y + VEL + vermelho.height < ALTURA: #BAIXO
-        vermelho.y += VEL
-
-def azul_mov(keys_pressed,azul):
-    if keys_pressed[pygame.K_LEFT] and azul.x - VEL > 0: #ESQUERDA
-        azul.x -= VEL
-    if keys_pressed[pygame.K_RIGHT] and azul.x + VEL + azul.width < LARGURA: #DIREITA
-        azul.x += VEL
-    if keys_pressed[pygame.K_UP] and azul.y - VEL > 0: #CIMA
-        azul.y -= VEL
-    if keys_pressed[pygame.K_DOWN] and azul.y + VEL + azul.height < ALTURA: #BAIXO
-        azul.y += VEL
+def colisao(player, objeto):
+    objetos_colididos = []
+    if pygame.sprite.collide(player,objeto):
+        objetos_colididos.append(objeto)
+        return True
 
 def main():
-    vermelho = pygame.Rect(x, y, LARG_PERS, ALTU_PERS)
-    azul = pygame.Rect(x_azul, y_azul, LARG_PERS, ALTU_PERS)
 
     clock = pygame.time.Clock()
     rodar = True
@@ -143,6 +151,7 @@ def main():
 
     player = Player(x, y, LARG_PERS, ALTU_PERS)
 
+    anel = Objeto(x_azul, y_azul, LARG_PERS, ALTU_PERS)
 
     while rodar:
         clock.tick(FPS)
@@ -153,20 +162,18 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 rodar = False
-
-        keys_pressed = pygame.key.get_pressed()
-
-        vermelho_mov(keys_pressed,vermelho)
-        azul_mov(keys_pressed,azul)
         
         player.loop(FPS)
+
         mover(player)
 
-        draw_janela(vermelho,azul,texto_formatado,background, player)
+        draw_janela(texto_formatado, background, player, anel)
 
-        if vermelho.colliderect(azul):
+        if player.rect.colliderect(anel):
+            anel.rect.x = 100000
             pontos += 1
             som_colisao.play()
+        update_rect = background.blit(background, anel, anel)
 
     pygame.quit()
 
